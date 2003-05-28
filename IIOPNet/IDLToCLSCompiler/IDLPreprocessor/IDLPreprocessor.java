8c003ad03ff9f8f344404ff00722b4c47e92ee57
Dominic Ullmann <dullmann@users.sourceforge.net>
2003-05-28 07:09:15 +0000

    replaced tabs by spaces
    git-svn-id: https://iiop-net.svn.sourceforge.net/svnroot/iiop-net/trunk@65 b113d865-ab28-0410-9ae9-d2690f63996d

--- IIOPNet/IDLToCLSCompiler/IDLPreprocessor/IDLPreprocessor.java
+++ IIOPNet/IDLToCLSCompiler/IDLPreprocessor/IDLPreprocessor.java
@@ -47,16 +47,16 @@ import java.util.StringTokenizer;
  * Summary description for Class1.
  */
 public class IDLPreprocessor {
-	
+    
     #region IFields
 
-	private BufferedReader m_fileStream;
+    private BufferedReader m_fileStream;
 
     private InputStream m_processed = null;
 
-    	/** stores the preprocessor result */
-	private PrintWriter m_outputStream;
-	private ByteArrayOutputStream m_outData;
+    /** stores the preprocessor result */
+    private PrintWriter m_outputStream;
+    private ByteArrayOutputStream m_outData;
 
     private int m_ifOpen = 0;
 
@@ -64,149 +64,159 @@ public class IDLPreprocessor {
 
     #endregion IFields
     #region IConstructors
-	
-	/** @param resolveInclude should includefiles be included or not
-	 */
-	public IDLPreprocessor(File toProcess) throws Exception, java.io.IOException {
-		m_defined = new Hashtable();
-		Init(toProcess);
-	}
+    
+    /** @param resolveInclude should includefiles be included or not
+     */
+    public IDLPreprocessor(File toProcess) throws Exception, java.io.IOException {
+        m_defined = new Hashtable();
+        Init(toProcess);
+    }
 
-	/** internal constructor to resolve included files */
-	private IDLPreprocessor(File toProcess, Hashtable symbols) throws Exception, java.io.IOException {
-		m_defined = symbols;
-		Init(toProcess);
-	}
+    /** internal constructor to resolve included files */
+    private IDLPreprocessor(File toProcess, Hashtable symbols) throws Exception, java.io.IOException {
+        m_defined = symbols;
+        Init(toProcess);
+    }
 
     #endregion IConstructors
     #region IMethods
 
-	private void Init(File toProcess) throws Exception, java.io.IOException {
-		m_fileStream = new BufferedReader(new FileReader(toProcess));
-		m_outData = new ByteArrayOutputStream();
-		m_outputStream = new PrintWriter(new OutputStreamWriter(m_outData), true);
-	}
+    private void Init(File toProcess) throws Exception, java.io.IOException {
+        m_fileStream = new BufferedReader(new FileReader(toProcess));
+        m_outData = new ByteArrayOutputStream();
+        m_outputStream = new PrintWriter(new OutputStreamWriter(m_outData), true);
+    }
 
-	/** processes the file */
-	public void Process() throws Exception, java.io.IOException {
-		String currentLine = m_fileStream.readLine();
-		while (currentLine != null) {
-			currentLine = currentLine.trim();
-			if (currentLine.startsWith("#include"))	{
-				ProcessInclude(currentLine);
-			} else if (currentLine.startsWith("#define")) {
-				ProcessDefine(currentLine);
-			} else if (currentLine.startsWith("#ifndef")) {
-				ProcessIfNDef(currentLine);
-			} else if (currentLine.startsWith("#endif")) {
-				ProcessEndIf(currentLine);
-			} else {
-				// write the current line to the output stream
-				m_outputStream.println(currentLine);
-			}									   
-			currentLine = m_fileStream.readLine();
-		}
-		m_fileStream.close();
-		m_outputStream.println(""); // add a newline at the end, because parser needs at least one line
-		m_processed = new ByteArrayInputStream(m_outData.toByteArray());
-	}
+    /** processes the file */
+    public void Process() throws Exception, java.io.IOException {
+        String currentLine = m_fileStream.readLine();
+        while (currentLine != null) {
+            currentLine = currentLine.trim();
+            if (currentLine.startsWith("#include"))    {
+                ProcessInclude(currentLine);
+            } else if (currentLine.startsWith("#define")) {
+                ProcessDefine(currentLine);
+            } else if (currentLine.startsWith("#ifndef")) {
+                ProcessIfNDef(currentLine);
+            } else if (currentLine.startsWith("#endif")) {
+                ProcessEndIf(currentLine);
+            } else {
+                // write the current line to the output stream
+                m_outputStream.println(currentLine);
+            }                                       
+            currentLine = m_fileStream.readLine();
+        }
+        m_fileStream.close();
+        m_outputStream.println(""); // add a newline at the end, because parser needs at least one line
+        m_processed = new ByteArrayInputStream(m_outData.toByteArray());
+    }
 
-	
+    
 
-	/** gets the preprocessed file for further processing */
-	public InputStream GetProcessed() {
-		return m_processed;
-	}
+    /** gets the preprocessed file for further processing */
+    public InputStream GetProcessed() {
+        return m_processed;
+    }
 
-	#region implementation of the preprocessing actions
+    #region implementation of the preprocessing actions
 
-	private void ProcessInclude(String currentLine) throws Exception {
-		StringTokenizer tokenizer = new StringTokenizer(currentLine.trim());
-		if (tokenizer.countTokens() <= 1) { 
+    private void ProcessInclude(String currentLine) throws Exception {
+        StringTokenizer tokenizer = new StringTokenizer(currentLine.trim());
+        if (tokenizer.countTokens() <= 1) { 
             throw new Exception("include missing argument"); 
         }
-		tokenizer.nextToken(); // ignore #include
-		String fileToInclude = tokenizer.nextToken();
-		if (fileToInclude.startsWith("\"")) { fileToInclude = fileToInclude.substring(1); }
-		if (fileToInclude.endsWith("\"")) { fileToInclude = fileToInclude.substring(0, fileToInclude.length() - 1); }
+        tokenizer.nextToken(); // ignore #include
+        String fileToInclude = tokenizer.nextToken();
+        if (fileToInclude.startsWith("\"")) { 
+            fileToInclude = fileToInclude.substring(1); 
+        }
+        if (fileToInclude.endsWith("\"")) { 
+            fileToInclude = fileToInclude.substring(0, fileToInclude.length() - 1); 
+        }
 
-		File toInclude = new File(fileToInclude);
-		IDLPreprocessor includePreproc = new IDLPreprocessor(toInclude, m_defined);
-		includePreproc.Process();
-		InputStream result = includePreproc.GetProcessed();
-		// copy result into current output stream
-		CopyToOutputStream(result);
-	}
-	
-	private void CopyToOutputStream(InputStream input) throws Exception {
-		BufferedReader resultReader = new BufferedReader(new java.io.InputStreamReader(input));
-		String currentLine = resultReader.readLine();
-		while (currentLine != null) {
-			m_outputStream.println(currentLine);
-			currentLine = resultReader.readLine();
-		}
-		resultReader.close();
-	}	
-	
-	private void ProcessDefine(String currentLine) throws Exception {
+        File toInclude = new File(fileToInclude);
+        IDLPreprocessor includePreproc = new IDLPreprocessor(toInclude, m_defined);
+        includePreproc.Process();
+        InputStream result = includePreproc.GetProcessed();
+        // copy result into current output stream
+        CopyToOutputStream(result);
+    }
+    
+    private void CopyToOutputStream(InputStream input) throws Exception {
+        BufferedReader resultReader = new BufferedReader(new java.io.InputStreamReader(input));
+        String currentLine = resultReader.readLine();
+        while (currentLine != null) {
+            m_outputStream.println(currentLine);
+            currentLine = resultReader.readLine();
+        }
+        resultReader.close();
+    }    
+    
+    private void ProcessDefine(String currentLine) throws Exception {
 
-		StringTokenizer tokenizer = new StringTokenizer(currentLine.trim());
-		if (tokenizer.countTokens() <= 1) { 
+        StringTokenizer tokenizer = new StringTokenizer(currentLine.trim());
+        if (tokenizer.countTokens() <= 1) { 
             throw new Exception("define missing argument"); 
         }
-		if (tokenizer.countTokens() > 3) { 
+        if (tokenizer.countTokens() > 3) { 
             throw new Exception("too much tokens in define directive"); 
         }
-		tokenizer.nextToken(); // ignore #ifndef token
-		String define = tokenizer.nextToken();
-				
-		String value = "";
-		if (tokenizer.hasMoreTokens()) {
-			value = tokenizer.nextToken();
-		}
-		if (m_defined.containsKey(define)) {
-			throw new Exception("redefinition of a variable: " + define);
-		}
-		m_defined.put(define, value);
-		System.Diagnostics.Debug.WriteLine("defined symbol in preproc: " + define);
-	}	
-	
-	private void ProcessIfNDef(String currentLine) throws Exception {
-		m_ifOpen++;
-		StringTokenizer tokenizer = new StringTokenizer(currentLine.trim());
-		if (tokenizer.countTokens() <= 1) { 
+        tokenizer.nextToken(); // ignore #ifndef token
+        String define = tokenizer.nextToken();
+                
+        String value = "";
+        if (tokenizer.hasMoreTokens()) {
+            value = tokenizer.nextToken();
+        }
+        if (m_defined.containsKey(define)) {
+            throw new Exception("redefinition of a variable: " + define);
+        }
+        m_defined.put(define, value);
+        System.Diagnostics.Debug.WriteLine("defined symbol in preproc: " + define);
+    }    
+    
+    private void ProcessIfNDef(String currentLine) throws Exception {
+        m_ifOpen++;
+        StringTokenizer tokenizer = new StringTokenizer(currentLine.trim());
+        if (tokenizer.countTokens() <= 1) { 
             throw new Exception("ifndef missing argument"); 
         }
-		if (tokenizer.countTokens() > 2) { 
+        if (tokenizer.countTokens() > 2) { 
             throw new Exception("too much tokens in ifndef directive"); 
         }
-		tokenizer.nextToken(); // ignore #ifndef token
-		String define = tokenizer.nextToken();
-		if (m_defined.containsKey(define)) { // throw everything in block away
-			SearchForEndif();
-			m_ifOpen--;
-		}
-	}
+        tokenizer.nextToken(); // ignore #ifndef token
+        String define = tokenizer.nextToken();
+        if (m_defined.containsKey(define)) { // throw everything in block away
+            SearchForEndif();
+            m_ifOpen--;
+        }
+    }
 
-	private void ProcessEndIf(String currentLine) throws Exception {
-		m_ifOpen--;
-		if (m_ifOpen < 0) { 
+    private void ProcessEndIf(String currentLine) throws Exception {
+        m_ifOpen--;
+        if (m_ifOpen < 0) { 
             throw new Exception("too much endif's encountered"); 
         }
-	}
+    }
 
-	/** search for a matching end-if for an if tag, throwing away everything in between */
-	private void SearchForEndif() throws Exception {
-		int moreIfs = 1; // more if's encountered than endif
-		String currentLine = "";
-		while ((moreIfs > 0) && (currentLine != null)) {
-			if (currentLine.startsWith("#if")) { moreIfs++; }
-			if (currentLine.startsWith("#endif")) { moreIfs--; }
-			if (moreIfs > 0) { currentLine = m_fileStream.readLine().trim(); }
-		}
-	}	
+    /** search for a matching end-if for an if tag, throwing away everything in between */
+    private void SearchForEndif() throws Exception {
+        int moreIfs = 1; // more if's encountered than endif
+        String currentLine = "";
+        while ((moreIfs > 0) && (currentLine != null)) {
+            if (currentLine.startsWith("#if")) {
+                moreIfs++; 
+            }
+            if (currentLine.startsWith("#endif")) { 
+                moreIfs--; 
+            }
+            if (moreIfs > 0) { 
+                currentLine = m_fileStream.readLine().trim(); 
+            }
+        }
+    }    
 
-	#endregion implementation of the preprocessing actions
+    #endregion implementation of the preprocessing actions
 
     #endregion IMethods
 
