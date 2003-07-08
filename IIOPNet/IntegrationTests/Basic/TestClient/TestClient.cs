3a7d74ab489bf0049c6b0b4b2eb38c11cac73bd8
Dominic Ullmann <dullmann@users.sourceforge.net>
2003-07-08 13:46:30 +0000

    initial version
    git-svn-id: https://iiop-net.svn.sourceforge.net/svnroot/iiop-net/trunk@218 b113d865-ab28-0410-9ae9-d2690f63996d

---
+++ IIOPNet/IntegrationTests/Basic/TestClient/TestClient.cs
@@ -0,0 +1,94 @@
+/* TestClient.cs
+ *
+ * Project: IIOP.NET
+ * IntegrationTests
+ *
+ * WHEN      RESPONSIBLE
+ * 08.07.03  Dominic Ullmann (DUL), dominic.ullmann -at- elca.ch
+ *
+ * Copyright 2003 ELCA Informatique SA
+ * Av. de la Harpe 22-24, 1000 Lausanne 13, Switzerland
+ * www.elca.ch
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+
+using System;
+using System.Runtime.Remoting.Channels;
+using NUnit.Framework;
+using Ch.Elca.Iiop;
+using Ch.Elca.Iiop.Services;
+using omg.org.CosNaming;
+
+namespace Ch.Elca.Iiop.IntegrationTests {
+
+    [TestFixture]
+    public class TestClient {
+
+        #region IFields
+
+        private IiopClientChannel m_channel;
+
+        private TestService m_testService;
+
+
+        #endregion IFields
+
+        [SetUp]
+        public void SetupEnvironment() {
+            // register the channel
+            m_channel = new IiopClientChannel();
+            ChannelServices.RegisterChannel(m_channel);
+
+            // access COS nameing service
+            CorbaInit init = CorbaInit.GetInit();
+            NamingContext nameService = init.GetNameService("localhost", 8087);
+            NameComponent[] name = new NameComponent[] { new NameComponent("test", "") };
+            // get the reference to the test-service
+            m_testService = (TestService)nameService.resolve(name);
+        }
+
+        [TearDown]
+        public void TearDownEnvironment() {
+            m_testService = null;
+            // unregister the channel            
+            ChannelServices.UnregisterChannel(m_channel);
+        }
+
+        [Test]
+        public void TestDouble() {
+            System.Double arg = 1.23;
+            System.Double result = m_testService.TestIncDouble(arg);
+            Assertion.AssertEquals(arg + 1, result);
+        }
+
+        [Test]
+        public void TestFloat() {
+            System.Single arg = 1.23f;
+            System.Single result = m_testService.TestIncFloat(arg);
+            Assertion.AssertEquals(arg + 1, result);
+        }
+        
+        [Test]
+        public void TestByte() {
+            System.Byte arg = 1;
+            System.Byte result = m_testService.TestIncByte(arg);
+            Assertion.AssertEquals(arg + 1, result);
+        }
+
+    }
+
+}
