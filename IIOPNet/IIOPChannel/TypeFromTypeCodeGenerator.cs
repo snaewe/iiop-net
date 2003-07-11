0c97aa8fd458bc563b95b818182a685004b6e82b
Dominic Ullmann <dullmann@users.sourceforge.net>
2003-07-11 12:47:13 +0000

    corrected creation date
    git-svn-id: https://iiop-net.svn.sourceforge.net/svnroot/iiop-net/trunk@247 b113d865-ab28-0410-9ae9-d2690f63996d

--- IIOPNet/IIOPChannel/TypeFromTypeCodeGenerator.cs
+++ IIOPNet/IIOPChannel/TypeFromTypeCodeGenerator.cs
@@ -4,7 +4,7 @@
  * IIOPChannel
  * 
  * WHEN      RESPONSIBLE
- * 24.01.03  Dominic Ullmann (DUL), dominic.ullmann -at- elca.ch
+ * 11.07.03  Dominic Ullmann (DUL), dominic.ullmann -at- elca.ch
  * 
  * Copyright 2003 Dominic Ullmann
  *
