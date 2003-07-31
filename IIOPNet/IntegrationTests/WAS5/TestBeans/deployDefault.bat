28d333be75759586d902fa219448f7802281f0f8
Patrik Reali <patrik_reali@users.sourceforge.net>
2003-07-31 07:44:11 +0000

    improvement, safe for paths containing spaces
    git-svn-id: https://iiop-net.svn.sourceforge.net/svnroot/iiop-net/trunk@317 b113d865-ab28-0410-9ae9-d2690f63996d

--- IIOPNet/IntegrationTests/WAS5/TestBeans/deployDefault.bat
+++ IIOPNet/IntegrationTests/WAS5/TestBeans/deployDefault.bat
@@ -1,1 +1,1 @@
-%WAS_HOME%\bin\wsadmin -conntype none -c "$AdminApp install bin/WAS5Integration.ear {-appname WAS5Integration -usedefaultbindings -deployejb }"
+"%WAS_HOME%\bin\wsadmin" -conntype none -c "$AdminApp install bin/WAS5Integration.ear {-appname WAS5Integration -usedefaultbindings -deployejb }"
