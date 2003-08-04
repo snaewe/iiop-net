1f5d2c2f3b167dd4835cbee30972419a5def21da
Dominic Ullmann <dullmann@users.sourceforge.net>
2003-08-04 15:52:19 +0000

    initial version
    git-svn-id: https://iiop-net.svn.sourceforge.net/svnroot/iiop-net/trunk@337 b113d865-ab28-0410-9ae9-d2690f63996d

---
+++ IIOPNet/Examples/EJBChatRoom/WLS6.1/net/Client.cs
@@ -0,0 +1,80 @@
+/* Client.cs
+ *
+ * Project: IIOP.NET
+ * Examples
+ *
+ * WHEN      RESPONSIBLE
+ * 22.07.03  Dominic Ullmann (DUL), dominic.ullmann -at- elca.ch
+ *
+ * Copyright 2003 ELCA Informatique SA
+ * Av. de la Harpe 22-24, 1000 Lausanne 13, Switzerland
+ * www.elca.ch
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+
+using System;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting;
+using System.Windows.Forms;
+using Ch.Elca.Iiop;
+using Ch.Elca.Iiop.Services;
+using omg.org.CosNaming;
+
+namespace ch.elca.iiop.demo.ejbChatroom {
+
+    public class Client {
+
+        [STAThread]
+        public static void Main(string[] args) {
+            try {
+
+                string ejbNameServiceHost = "localhost";
+                int ejbNameServicePort = 7001;
+                if (args.Length > 0) {
+                    ejbNameServiceHost = args[0];
+                }
+                if (args.Length > 1) {
+                    ejbNameServicePort = Int32.Parse(args[1]);
+                }
+
+                // the port the callback is listening on
+                int callbackPort = 0; // auto assign
+                if (args.Length > 2) {
+                    callbackPort = Int32.Parse(args[2]);
+                }
+            
+                IiopChannel channel = new IiopChannel(callbackPort);
+                ChannelServices.RegisterChannel(channel);
+
+                RmiIiopInit init = new RmiIiopInit(ejbNameServiceHost, ejbNameServicePort);
+                NamingContext nameService = init.GetNameService();
+
+                NameComponent[] name = new NameComponent[] { new NameComponent("demo", ""),
+                                                             new NameComponent("chatroomHome", "") };
+                // get the chatroom home interface
+                ChatroomHome home = (ChatroomHome) nameService.resolve(name);
+                Chatroom chatroom = home.create();
+
+                Application.Run(new Chatform(chatroom));
+
+            } catch (Exception e) {
+                Console.WriteLine("exception: " + e);
+            }
+        }
+
+    }
+}
---
+++ IIOPNet/Examples/EJBChatRoom/WLS6.1/net/MessageImpl.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace ch.elca.iiop.demo.ejbChatroom {
+	
+    /// <summary>
+	/// Implementation of the CORBA value type Message
+	/// </summary>
+	[Serializable]
+    public class MessageImpl : Message {
+
+        public MessageImpl() {
+		}
+
+        public MessageImpl(string originator, string msg) {
+            m_originator = originator;
+            m_msg = msg;
+        }
+
+        public string fromUser {
+            get {
+                return m_originator;
+            }
+        }
+
+        public string msg {
+            get {
+                return m_msg;
+            }
+        }
+
+	}
+}
