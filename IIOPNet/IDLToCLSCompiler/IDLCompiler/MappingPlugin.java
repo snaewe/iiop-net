ea46d1048a2477393e259c75fc740f06f25a2f70
Dominic Ullmann <dullmann@users.sourceforge.net>
2003-08-21 08:21:06 +0000

    validating custom mapping plugin xml config files against schema
    git-svn-id: https://iiop-net.svn.sourceforge.net/svnroot/iiop-net/trunk@426 b113d865-ab28-0410-9ae9-d2690f63996d

--- IIOPNet/IDLToCLSCompiler/IDLCompiler/MappingPlugin.java
+++ IIOPNet/IDLToCLSCompiler/IDLCompiler/MappingPlugin.java
@@ -30,17 +30,31 @@
 
 package Ch.Elca.Iiop.IdlCompiler.Action;
 
+import System.Reflection.Assembly;
 import System.Xml.XmlDocument;
 import System.Xml.XmlNode;
 import System.Xml.XmlElement;
 import System.Xml.XmlNodeList;
+import System.Xml.XmlTextReader;
+import System.Xml.XmlValidatingReader;
+import System.Xml.Schema.XmlSchema;
+import System.Xml.Schema.ValidationEventHandler;
+import System.Xml.Schema.ValidationEventArgs;
+import System.Xml.Schema.XmlSchemaException;
+
 import System.IO.FileInfo;
 import System.IO.FileStream;
 import System.IO.FileMode;
+import System.IO.Stream;
 
 public class CompilerMappingPlugin {
 	
-	#region SIFields
+    #region Constants
+
+    private final static String XSD_RESOURCE_NAME = "MappingPluginSchema.xsd";
+
+    #endregion Constants
+    #region SIFields
 	
 	private static CompilerMappingPlugin s_mappingPlugin = new CompilerMappingPlugin();
 	
@@ -48,12 +62,20 @@ public class CompilerMappingPlugin {
 	#region IFields
 	
 	private System.Collections.Hashtable m_mappingTable = new System.Collections.Hashtable();
+
+    private XmlSchema m_mappingPluginSchema;
 	
 	#endregion IFields
 	#region IConstructors
 	
 	private CompilerMappingPlugin() {
-		
+        // load xsd schema from compiler assembly
+        Assembly asm = GetType().get_Assembly();
+        Stream xsdStream = asm.GetManifestResourceStream(XSD_RESOURCE_NAME);
+        if (xsdStream != null) {
+            m_mappingPluginSchema = XmlSchema.Read(xsdStream, new ValidationEventHandler(this.OnValidationEvent));
+            xsdStream.Close();
+        }
 	}
 	
 	#endregion IConstructors
@@ -65,50 +87,71 @@ public class CompilerMappingPlugin {
 	
 	#endregion SMethods
 	#region IMethods
-	
+
+	/** 
+     * is called on errors in schema.
+     * remark: should not be called, because schema is ok
+     */ 
+    private void OnValidationEvent(System.Object sender, ValidationEventArgs e) {
+    }
+
 	/**
      * checks, if a special mapping for the idl-type idlType exists.
      * @param idlFullName the fully qualified name of the type to check
      */
-     public boolean IsCustomMappingPresentForIdl(System.String idlFullName) {
-         return m_mappingTable.Contains(idlFullName);
-     }
+    public boolean IsCustomMappingPresentForIdl(System.String idlFullName) {
+        return m_mappingTable.Contains(idlFullName);
+    }
      
-     /**
-      * specifies a special mapping from idl to cls, e.g. java.util.ArrayList -> CLS System.Collections.ArrayList
-      */
-     public void AddMapping(System.String idlFullName, System.Type clsType) {
-     	if (!m_mappingTable.Contains(idlFullName)) {
-     		m_mappingTable.Add(idlFullName, clsType);
-     	}
-     }
-     
-     public void AddMappingsFromFile(FileInfo configFile) {
-         // load the xml-file
-         XmlDocument doc = new XmlDocument();
-         FileStream file = new FileStream(configFile.get_FullName(), FileMode.Open);
-         doc.Load(file);
-         file.Close();
-         // process the file
-         XmlNodeList elemList = doc.GetElementsByTagName("mapping");
-         for (int i = 0; i < elemList.get_Count(); i++) {
-             XmlNode elem = elemList.get_ItemOf(i);
-             XmlElement idlTypeName = elem.get_Item("idlTypeName");
-             XmlElement clsTypeAsqName = elem.get_Item("clsType");
-             // idl-type name:
-             System.String idlType = idlTypeName.get_InnerText();
-             // clsType:
-             System.Type clsType = System.Type.GetType(clsTypeAsqName.get_InnerText(), true);
-             AddMapping(idlType, clsType);
+    /**
+     * specifies a special mapping from idl to cls, e.g. java.util.ArrayList -> CLS System.Collections.ArrayList
+     */
+    public void AddMapping(System.String idlFullName, System.Type clsType) {
+        if (!m_mappingTable.Contains(idlFullName)) {
+     	    m_mappingTable.Add(idlFullName, clsType);
          }
-     }
+    }
      
-     /**
-      * returns the CLS type to which the given idl type should be mapped to
-      */
-     public System.Type GetMappingForIdl(System.String idlFullName) {
-     	return (System.Type)m_mappingTable.get_Item(idlFullName);
-     }
+    public void AddMappingsFromFile(FileInfo configFile) {
+        // check schema ok
+        if (m_mappingPluginSchema == null) {
+            throw new RuntimeException("schema loading problem");
+        }
+        // load the xml-file
+        XmlDocument doc = new XmlDocument();
+        FileStream stream = new FileStream(configFile.get_FullName(), FileMode.Open);
+        XmlTextReader textReader = new XmlTextReader(stream);            
+        XmlValidatingReader validatingReader = new XmlValidatingReader(textReader);
+        try {
+            validatingReader.get_Schemas().Add(m_mappingPluginSchema);
+            try {
+                doc.Load(validatingReader);
+            } catch (XmlSchemaException e) {
+                throw new RuntimeException("validation failed: " + e.ToString());
+            }
+            // process the file
+            XmlNodeList elemList = doc.GetElementsByTagName("mapping");
+            for (int i = 0; i < elemList.get_Count(); i++) {
+                XmlNode elem = elemList.get_ItemOf(i);
+                XmlElement idlTypeName = elem.get_Item("idlTypeName");
+                XmlElement clsTypeAsqName = elem.get_Item("clsType");
+                // idl-type name:
+                System.String idlType = idlTypeName.get_InnerText();
+                // clsType:
+                System.Type clsType = System.Type.GetType(clsTypeAsqName.get_InnerText(), true);
+                AddMapping(idlType, clsType);
+            }
+        } finally {
+            validatingReader.Close();            
+        }
+    }
+     
+    /**
+     * returns the CLS type to which the given idl type should be mapped to
+     */
+    public System.Type GetMappingForIdl(System.String idlFullName) {
+        return (System.Type)m_mappingTable.get_Item(idlFullName);
+    }
 
 	
 	#endregion IMethods
