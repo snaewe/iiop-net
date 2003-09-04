fb1723b83099960548181447ff20ef834ee6ee58
Dominic Ullmann <dullmann@users.sourceforge.net>
2003-09-04 10:28:51 +0000

    added some options to lauch: - start non-minimized - working directory - help
    git-svn-id: https://iiop-net.svn.sourceforge.net/svnroot/iiop-net/trunk@483 b113d865-ab28-0410-9ae9-d2690f63996d

--- IIOPNet/IntegrationTests/Utils/launch.cs
+++ IIOPNet/IntegrationTests/Utils/launch.cs
@@ -30,23 +30,68 @@
 
 using System;
 using System.Diagnostics;
+using System.IO;
 
 public class Launch {
 	public static void Main(String[] args) {
-		ProcessStartInfo startInfo = null;
-		if (args.Length <= 0) {
-			Console.WriteLine("Usage:");
-			Console.WriteLine("Lauch name [args]");
-			Environment.Exit(2);
-		} else if (args.Length == 1) {
-			startInfo = new ProcessStartInfo(args[0]);
-		} else {
-			String parameters = String.Join(" ", args, 1, args.Length-1);
-			startInfo = new ProcessStartInfo(args[0], parameters);
-		}
-		startInfo.WindowStyle = ProcessWindowStyle.Minimized;
+		ProcessStartInfo startInfo = ParseArgs(args);
+		
 		Process p = Process.Start(startInfo);
 		Console.WriteLine(p.Id.ToString());
-
 	}
+	
+	public static ProcessStartInfo ParseArgs(String[] args) {
+        int i = 0;
+        ProcessStartInfo startInfo = new ProcessStartInfo();
+		startInfo.WindowStyle = ProcessWindowStyle.Minimized;
+
+        while ((i < args.Length) && (args[i].StartsWith("-"))) {
+            if (args[i].Equals("-h")) {
+                HowTo();
+                Environment.Exit(0);
+            } else if (args[i].Equals("-d")) {
+                i++;
+                if (i == args.Length) {
+                	Error("Error: option argument missing for -d option");
+                }
+                startInfo.WorkingDirectory = new DirectoryInfo(args[i++]).FullName;
+            } else if (args[i].Equals("-w")) {
+                startInfo.WindowStyle = ProcessWindowStyle.Normal;
+                i++;
+            } else {
+            	Error(String.Format("Error: invalid option {0}", args[i]));
+            }
+        }
+
+   		if (args.Length - i <= 0) {
+   			Error("Error: programm to launch is missing");
+   		} else if (args.Length - i == 1) {
+   		    startInfo.FileName = args[i];
+   		} else {
+   		    startInfo.FileName = args[i];
+   		    i++;
+   		    String parameters = String.Join(" ", args, i, (args.Length-i));
+   		    startInfo.Arguments = parameters;
+   		}
+
+        return startInfo;
+	
+	}
+	
+    public static void HowTo() {
+        Console.WriteLine("Usage:");
+        Console.WriteLine("Lauch [options] name [args]");
+        Console.WriteLine();
+        Console.WriteLine("options are:");
+        Console.WriteLine("-h              help");
+        Console.WriteLine("-d directory    the working directory for programm to start");
+        Console.WriteLine("-w              start not minimized, but normal");
+    }
+    
+    public static void Error(String message) {
+        Console.WriteLine(message);
+        Console.WriteLine();
+        HowTo();
+        Environment.Exit(2);
+    }
 }
