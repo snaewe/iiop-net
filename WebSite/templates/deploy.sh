15c893d3d2b17b0b5f4833a8a7335508e1700831
Patrik Reali <patrik_reali@users.sourceforge.net>
2003-09-24 08:02:30 +0000

    typo corrected
    git-svn-id: https://iiop-net.svn.sourceforge.net/svnroot/iiop-net/trunk@509 b113d865-ab28-0410-9ae9-d2690f63996d

--- WebSite/templates/deploy.sh
+++ WebSite/templates/deploy.sh
@@ -2,13 +2,13 @@ dest=..
 
 
 for name in *.html; do
-	cat layout1.html $name layout2.html >> $dest/$name
+	cat layout1.html $name layout2.html > $dest/$name
 done
 
 mkdir $dest/Tutorial
 
 for name in Tutorial/*.html; do
-	cat layout1.html $name layout2.html >> $dest/$name
+	cat layout1.html $name layout2.html > $dest/$name
 done
 
 rm $dest/layout1.html $dest/layout2.html
