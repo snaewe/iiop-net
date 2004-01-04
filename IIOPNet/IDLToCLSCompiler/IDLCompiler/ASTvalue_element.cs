8be80ee958f81fe94d3a7948c69a4aa7c3ba826c
Dominic Ullmann <dullmann@users.sourceforge.net>
2004-01-04 15:52:09 +0000

    adapt to coding convention
    git-svn-id: https://iiop-net.svn.sourceforge.net/svnroot/iiop-net/trunk@676 b113d865-ab28-0410-9ae9-d2690f63996d

--- IIOPNet/IDLToCLSCompiler/IDLCompiler/ASTvalue_element.cs
+++ IIOPNet/IDLToCLSCompiler/IDLCompiler/ASTvalue_element.cs
@@ -4,25 +4,31 @@ using System;
 
 namespace parser {
 
-public class ASTvalue_element : SimpleNode {
-  public ASTvalue_element(int id) : base(id) {
-  }
+    public class ASTvalue_element : SimpleNode {
+        
+        #region IConstructors
+        
+        public ASTvalue_element(int id) : base(id) {
+        }
 
-  public ASTvalue_element(IDLParser p, int id) : base(p, id) {
-  }
+        public ASTvalue_element(IDLParser p, int id) : base(p, id) {
+        }
+        
+        #endregion IConstructors
+        #region IMethods
 
-
-  /** Accept the visitor. **/
-  public override Object jjtAccept(IDLParserVisitor visitor, Object data) {
-    return visitor.visit(this, data);
-  }
+        /** Accept the visitor. **/
+        public override Object jjtAccept(IDLParserVisitor visitor, Object data) {
+            return visitor.visit(this, data);
+        }
   
-  public override string GetEmbedderDesc() {
-    return ((SimpleNode)jjtGetParent()).GetEmbedderDesc();
-  }
+        public override string GetEmbedderDesc() {
+            return ((SimpleNode)jjtGetParent()).GetEmbedderDesc();
+        }
+        
+        #endregion IConstructors
 
-}
-
+    }
 
 }
 
