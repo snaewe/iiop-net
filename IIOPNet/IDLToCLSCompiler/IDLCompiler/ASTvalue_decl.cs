cd9cf678092b58cafcdb0fd85a80534954419e75
Dominic Ullmann <dullmann@users.sourceforge.net>
2004-01-04 11:46:35 +0000

    adapt to coding convention
    git-svn-id: https://iiop-net.svn.sourceforge.net/svnroot/iiop-net/trunk@675 b113d865-ab28-0410-9ae9-d2690f63996d

--- IIOPNet/IDLToCLSCompiler/IDLCompiler/ASTvalue_decl.cs
+++ IIOPNet/IDLToCLSCompiler/IDLCompiler/ASTvalue_decl.cs
@@ -4,32 +4,38 @@ using System;
 
 namespace parser {
 
-public class ASTvalue_decl : SimpleNode {
-  public ASTvalue_decl(int id) : base(id) {
-  }
+    public class ASTvalue_decl : SimpleNode {
+                
+        #region IConstructors
 
-  public ASTvalue_decl(IDLParser p, int id) : base(p, id) {
-  }
+        public ASTvalue_decl(int id) : base(id) {
+        }
+
+        public ASTvalue_decl(IDLParser p, int id) : base(p, id) {
+        }
+        
+        #endregion IConstructors
+        #region IMethods
 
 
-  /** Accept the visitor. **/
-  public override Object jjtAccept(IDLParserVisitor visitor, Object data) {
-    return visitor.visit(this, data);
-  }
+        /** Accept the visitor. **/
+        public override Object jjtAccept(IDLParserVisitor visitor, Object data) {
+            return visitor.visit(this, data);
+        }
   
-  public override string GetIdentification() {
-    return "value " + ((SimpleNode)jjtGetChild(0)).GetIdentification() + "(in " + 
-            ((SimpleNode)jjtGetParent()).GetEmbedderDesc() + ")";
-  }    
+        public override string GetIdentification() {
+            return "value " + ((SimpleNode)jjtGetChild(0)).GetIdentification() + "(in " + 
+                   ((SimpleNode)jjtGetParent()).GetEmbedderDesc() + ")";
+        }    
   
-  public override string GetEmbedderDesc() {
-    return ((SimpleNodeWithIdent)jjtGetChild(0)).getIdent() + 
-        " (in " + ((SimpleNode)jjtGetParent()).GetEmbedderDesc() + ")";
-  }
-
+        public override string GetEmbedderDesc() {
+            return ((SimpleNodeWithIdent)jjtGetChild(0)).getIdent() + 
+                   " (in " + ((SimpleNode)jjtGetParent()).GetEmbedderDesc() + ")";
+        }
+        
+        #endregion IMethods
   
-}
-
+    }
 
 }
 
