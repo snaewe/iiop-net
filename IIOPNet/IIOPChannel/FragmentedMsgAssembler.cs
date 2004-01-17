0a1aa7b39066ff09b4d5cb1b55559aa388706ae7
Dominic Ullmann <dullmann@users.sourceforge.net>
2004-01-17 14:15:13 +0000

    replaces tabs by spaces
    git-svn-id: https://iiop-net.svn.sourceforge.net/svnroot/iiop-net/trunk@689 b113d865-ab28-0410-9ae9-d2690f63996d

--- IIOPNet/IIOPChannel/FragmentedMsgAssembler.cs
+++ IIOPNet/IIOPChannel/FragmentedMsgAssembler.cs
@@ -34,202 +34,202 @@ using Ch.Elca.Iiop.Cdr;
 
 namespace Ch.Elca.Iiop {
 
-	/// <summary>
-	/// this class manages and assembles fragmented 
-	/// messages for one connection
-	/// </summary>
-	internal sealed class FragmentedMsgAssembler {
-	
-		#region Types
-		
-		private class FragmentedMsgDesc {
-			
-			#region IFields 
-			
-			private GiopHeader m_header;
-			private Stream m_target = new MemoryStream();
-			
-			#endregion IFields
-			#region IConstructors
-			
-			internal FragmentedMsgDesc(GiopHeader header) {
-				m_header = header;	
-			}
-			
-			#endregion IConstructors
-			#region IProperties
-			
-			internal GiopHeader Header {
-				get { 
-					return m_header;
-				}
-			}
-			
-			internal Stream Target {
-				get {
-					return m_target;
-				}
-			}
-			
-			#endregion IProperties
-			
-		}
-		
-		#endregion Types
-		#region Constants
-	
-		// GIOP 1.1 doen't support req-id in fragments -> use instead this key to retrieve
-		// msg from Hashtable
-		private object m_giop11Key = new object();
-	
-		#endregion Constants
-		#region IFields
-	
-		private Hashtable m_fragmentedMsgs = new Hashtable();
-	
-		#endregion IFields
-		#region IMethods
-	
-		private void AddFragmentInternal(object key, CdrInputStream source,
-		                                 uint contentLength) {
-			lock(m_fragmentedMsgs.SyncRoot) {
-				FragmentedMsgDesc fragmentDesc = (FragmentedMsgDesc) m_fragmentedMsgs[key];
-				if (fragmentDesc == null) {
-					throw new IOException("illegal fragment");
-				}
-				// read payload of msg and copy into target stream
-				Stream target = fragmentDesc.Target;
-				byte[] content = source.ReadOpaque((int) contentLength);
-				target.Write(content, 0, content.Length);
-			}
-		}
-	
-		/// <summary>
-		/// completes the fragmented msg.
-		/// </summary>
-		/// <param name="key"></param>
-		/// <param name="source"></param>
-		/// <param name="contentLength"></param>
-		/// <param name="fullMsgHeader">returns the header of the complete msg as an out parameter</param>
-		/// <returns>the full msg as a stream</returns>
+    /// <summary>
+    /// this class manages and assembles fragmented 
+    /// messages for one connection
+    /// </summary>
+    internal sealed class FragmentedMsgAssembler {
+    
+        #region Types
+        
+        private class FragmentedMsgDesc {
+            
+            #region IFields 
+            
+            private GiopHeader m_header;
+            private Stream m_target = new MemoryStream();
+            
+            #endregion IFields
+            #region IConstructors
+            
+            internal FragmentedMsgDesc(GiopHeader header) {
+                m_header = header;    
+            }
+            
+            #endregion IConstructors
+            #region IProperties
+            
+            internal GiopHeader Header {
+                get { 
+                    return m_header;
+                }
+            }
+            
+            internal Stream Target {
+                get {
+                    return m_target;
+                }
+            }
+            
+            #endregion IProperties
+            
+        }
+        
+        #endregion Types
+        #region Constants
+    
+        // GIOP 1.1 doen't support req-id in fragments -> use instead this key to retrieve
+        // msg from Hashtable
+        private object m_giop11Key = new object();
+    
+        #endregion Constants
+        #region IFields
+    
+        private Hashtable m_fragmentedMsgs = new Hashtable();
+    
+        #endregion IFields
+        #region IMethods
+    
+        private void AddFragmentInternal(object key, CdrInputStream source,
+                                         uint contentLength) {
+            lock(m_fragmentedMsgs.SyncRoot) {
+                FragmentedMsgDesc fragmentDesc = (FragmentedMsgDesc) m_fragmentedMsgs[key];
+                if (fragmentDesc == null) {
+                    throw new IOException("illegal fragment");
+                }
+                // read payload of msg and copy into target stream
+                Stream target = fragmentDesc.Target;
+                byte[] content = source.ReadOpaque((int) contentLength);
+                target.Write(content, 0, content.Length);
+            }
+        }
+    
+        /// <summary>
+        /// completes the fragmented msg.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="source"></param>
+        /// <param name="contentLength"></param>
+        /// <param name="fullMsgHeader">returns the header of the complete msg as an out parameter</param>
+        /// <returns>the full msg as a stream</returns>
         private Stream FinishFragmentedMsgInternal(object key,
-		                                           CdrInputStream source,
-		                                           uint contentLength, 
+                                                   CdrInputStream source,
+                                                   uint contentLength, 
                                                    out GiopHeader fullMsgHeader) {
-			lock(m_fragmentedMsgs.SyncRoot) {
-				FragmentedMsgDesc fragmentDesc = (FragmentedMsgDesc) m_fragmentedMsgs[key];
-				if (fragmentDesc == null) {
-					throw new IOException("illegal fragment");
-				}
-				// write read of msg-content
-				Stream target = fragmentDesc.Target;
-				byte[] content = source.ReadOpaque((int) contentLength);
-				target.Write(content, 0, content.Length);
-				// write adapted header
-				target.Seek(0, SeekOrigin.Begin);
+            lock(m_fragmentedMsgs.SyncRoot) {
+                FragmentedMsgDesc fragmentDesc = (FragmentedMsgDesc) m_fragmentedMsgs[key];
+                if (fragmentDesc == null) {
+                    throw new IOException("illegal fragment");
+                }
+                // write read of msg-content
+                Stream target = fragmentDesc.Target;
+                byte[] content = source.ReadOpaque((int) contentLength);
+                target.Write(content, 0, content.Length);
+                // write adapted header
+                target.Seek(0, SeekOrigin.Begin);
                 GiopHeader newHeader = new GiopHeader(fragmentDesc.Header.Version.Major, 
                                                       fragmentDesc.Header.Version.Minor,
                                                       (byte)(fragmentDesc.Header.GiopFlags ^ GiopHeader.FRAGMENT_MASK),
                                                       fragmentDesc.Header.GiopType);
 
-				newHeader.WriteToStream(target, 
-				                        ((uint)target.Length) - GiopHeader.HEADER_LENGTH);
-				target.Seek(0, SeekOrigin.Begin);
-				
+                newHeader.WriteToStream(target, 
+                                        ((uint)target.Length) - GiopHeader.HEADER_LENGTH);
+                target.Seek(0, SeekOrigin.Begin);
+                
                 // remove for unfinished msg table
                 m_fragmentedMsgs.Remove(fragmentDesc);
                 
                 // the header of the full msg
                 fullMsgHeader = newHeader;
                 // return the complete result msg
-				return target;
-			}
-		}
-	
-		/// <summary>Start a new fragmented msg</summary>
-		internal void StartFragment(CdrInputStream source, GiopHeader header) {
-			lock(m_fragmentedMsgs.SyncRoot) {
-				FragmentedMsgDesc fragmentDesc = new FragmentedMsgDesc(header);
-				Stream target = fragmentDesc.Target;
-				CdrOutputStream cdrOut = new CdrOutputStreamImpl(target, header.GiopFlags,
-				                                                 header.Version);
-				// write placeholder header (is replaced during finish)
-				header.WriteToStream(cdrOut, header.ContentMsgLength);
-				object descKey = null;
-				int contentLength = (int) header.ContentMsgLength;
-			
-				if ((header.Version.Major == 1) && (header.Version.Minor == 1)) {
-					descKey = m_giop11Key;
-				} else if (!((header.Version.Major == 1) && (header.Version.Minor == 0))) {
-					// GIOP 1.2 or newer
-					uint reqId = source.ReadULong();
-					cdrOut.WriteULong(reqId);
-					descKey = reqId;
-					contentLength -= 4;
-				} else {
-					// no fragmentation allowed		
-					throw new IOException("fragmentation not allowed for GIOP 1.0");
-				}
-				
-				// write content to target stream
-				byte[] content = source.ReadOpaque(contentLength);
-				cdrOut.WriteOpaque(content);
+                return target;
+            }
+        }
+    
+        /// <summary>Start a new fragmented msg</summary>
+        internal void StartFragment(CdrInputStream source, GiopHeader header) {
+            lock(m_fragmentedMsgs.SyncRoot) {
+                FragmentedMsgDesc fragmentDesc = new FragmentedMsgDesc(header);
+                Stream target = fragmentDesc.Target;
+                CdrOutputStream cdrOut = new CdrOutputStreamImpl(target, header.GiopFlags,
+                                                                 header.Version);
+                // write placeholder header (is replaced during finish)
+                header.WriteToStream(cdrOut, header.ContentMsgLength);
+                object descKey = null;
+                int contentLength = (int) header.ContentMsgLength;
+            
+                if ((header.Version.Major == 1) && (header.Version.Minor == 1)) {
+                    descKey = m_giop11Key;
+                } else if (!((header.Version.Major == 1) && (header.Version.Minor == 0))) {
+                    // GIOP 1.2 or newer
+                    uint reqId = source.ReadULong();
+                    cdrOut.WriteULong(reqId);
+                    descKey = reqId;
+                    contentLength -= 4;
+                } else {
+                    // no fragmentation allowed        
+                    throw new IOException("fragmentation not allowed for GIOP 1.0");
+                }
+                
+                // write content to target stream
+                byte[] content = source.ReadOpaque(contentLength);
+                cdrOut.WriteOpaque(content);
 
                 // add to unfinished message table
                 m_fragmentedMsgs[descKey] = fragmentDesc;
-			}
-			
-		}
-	
-		internal void AddFragment(CdrInputStream source, 
-		                          GiopHeader header) {
-			if ((header.Version.Major == 1) && (header.Version.Minor == 1)) {
-				AddFragmentInternal(m_giop11Key, source, 
-				                    header.ContentMsgLength);
-			} else if (!((header.Version.Major == 1) && (header.Version.Minor == 0))) {
-				// GIOP 1.2 or newer: read request id from fragment msg header
-				uint reqId = source.ReadULong();
-				AddFragmentInternal(reqId, source, header.ContentMsgLength - 4);
-			} else {
-				// no fragmentation allowed		
-				throw new IOException("fragmentation not allowed for GIOP 1.0");
-			}		
-		}
-		
+            }
+            
+        }
+    
+        internal void AddFragment(CdrInputStream source, 
+                                  GiopHeader header) {
+            if ((header.Version.Major == 1) && (header.Version.Minor == 1)) {
+                AddFragmentInternal(m_giop11Key, source, 
+                                    header.ContentMsgLength);
+            } else if (!((header.Version.Major == 1) && (header.Version.Minor == 0))) {
+                // GIOP 1.2 or newer: read request id from fragment msg header
+                uint reqId = source.ReadULong();
+                AddFragmentInternal(reqId, source, header.ContentMsgLength - 4);
+            } else {
+                // no fragmentation allowed        
+                throw new IOException("fragmentation not allowed for GIOP 1.0");
+            }        
+        }
+        
 
-                internal Stream FinishFragmentedMsg(CdrInputStream source, 
-                                                    GiopHeader header) {
-                     return FinishFragmentedMsg(source, ref header);
-                }
+        internal Stream FinishFragmentedMsg(CdrInputStream source, 
+                                            GiopHeader header) {
+            return FinishFragmentedMsg(source, ref header);
+        }
 
-                /// <param name="header">
-                /// The fragment header of the last fragment. As an out parameter, the header of the complete msg is returned. 
-                /// </param>
-		internal Stream FinishFragmentedMsg(CdrInputStream source, 
-		                                    ref GiopHeader header) {
-			Stream result;
-			if ((header.Version.Major == 1) && (header.Version.Minor == 1)) {
-				result = FinishFragmentedMsgInternal(m_giop11Key, source,
-				                                     header.ContentMsgLength, out header);
-			} else if (!((header.Version.Major == 1) && (header.Version.Minor == 0))) {
-				// GIOP 1.2 or newer: read request id from fragment msg header
-				uint reqId = source.ReadULong();
-				result = FinishFragmentedMsgInternal(reqId, source,
-				                                     header.ContentMsgLength - 4, out header);
-			} else {
-				// no fragmentation allowed		
-				throw new IOException("fragmentation not allowed for GIOP 1.0");
-			}
-			return result;
-		}
-		
-		internal bool IsLastFragment(GiopHeader header) {
-			bool hasMore = ((header.GiopFlags & GiopHeader.FRAGMENT_MASK) > 0);
-			return !hasMore;
-		}
-	
-		#endregion IMethods
-		
-	}
-	
+        /// <param name="header">
+        /// The fragment header of the last fragment. As an out parameter, the header of the complete msg is returned. 
+        /// </param>
+        internal Stream FinishFragmentedMsg(CdrInputStream source, 
+                                            ref GiopHeader header) {
+            Stream result;
+            if ((header.Version.Major == 1) && (header.Version.Minor == 1)) {
+                result = FinishFragmentedMsgInternal(m_giop11Key, source,
+                                                     header.ContentMsgLength, out header);
+            } else if (!((header.Version.Major == 1) && (header.Version.Minor == 0))) {
+                // GIOP 1.2 or newer: read request id from fragment msg header
+                uint reqId = source.ReadULong();
+                result = FinishFragmentedMsgInternal(reqId, source,
+                                                     header.ContentMsgLength - 4, out header);
+            } else {
+                // no fragmentation allowed        
+                throw new IOException("fragmentation not allowed for GIOP 1.0");
+            }
+            return result;
+        }
+        
+        internal bool IsLastFragment(GiopHeader header) {
+            bool hasMore = ((header.GiopFlags & GiopHeader.FRAGMENT_MASK) > 0);
+            return !hasMore;
+        }
+    
+        #endregion IMethods
+        
+    }
+    
 }
