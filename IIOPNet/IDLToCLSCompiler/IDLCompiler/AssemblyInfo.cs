de6f49122acb3780499de322438cdac9976128a7
Dominic Ullmann <dullmann@users.sourceforge.net>
2007-02-04 09:00:58 +0000

    updated version number
    git-svn-id: https://iiop-net.svn.sourceforge.net/svnroot/iiop-net/trunk@1916 b113d865-ab28-0410-9ae9-d2690f63996d

--- IIOPNet/IDLToCLSCompiler/IDLCompiler/AssemblyInfo.cs
+++ IIOPNet/IDLToCLSCompiler/IDLCompiler/AssemblyInfo.cs
@@ -57,7 +57,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build
 // Numbers by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.9.0000.0")]
+[assembly: AssemblyVersion("1.9.0001.0")]
 [assembly: CLSCompliant(true)]
 
 //
--- IIOPNet/SSLPlugin/AssemblyInfo.cs
+++ IIOPNet/SSLPlugin/AssemblyInfo.cs
@@ -54,7 +54,7 @@ using System.Runtime.CompilerServices;
 // You can specify all values by your own or you can build default build and revision
 // numbers with the '*' character (the default):
 
-[assembly: AssemblyVersion("1.9.0000.0")]
+[assembly: AssemblyVersion("1.9.0001.0")]
 [assembly: CLSCompliant(true)]
 
 // The following attributes specify the key for the sign of your assembly. See the
